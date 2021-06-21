2fe3839f39c14e049f5699e1d840701f9677209f
Samuel Garcia <samfrontdev@gmail.com>
2021-06-21 13:34:35 -0500

    SearchBar added

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const API = "https://api.coinlore.net/api/tickers/";
+const container = document.querySelector("#container");
+const searchBar = document.querySelector("#searchBar");
+
+// EventListener para el searchBar
+
+searchBar.addEventListener("keyup", (e) => {
+  console.log(e.target.value);
+});
+
+// API
+
+window
+  .fetch(API)
+  // Procesar respuesta y convertirla en JSON
+  .then((respuesta) => respuesta.json())
+  // JSON -> DATA -> AL BROWSER
+  .then((responseJson) => {
+    const allItems = [];
+    responseJson.data.forEach((item) => {
+      const ranks = document.createElement("h2");
+      ranks.style.marginRight = "5px";
+      ranks.textContent = item.rank;
+
+      const title = document.createElement("h2");
+      title.textContent = item.name;
+
+      const price = document.createElement("p");
+      price.textContent = item.price_usd;
+
+      const textBox = document.createElement("div");
+      textBox.className = "textBox";
+      textBox.append(ranks, title);
+
+      const coinBox = document.createElement("div");
+      coinBox.className = "coinBox";
+      coinBox.append(textBox, price);
+
+      container.appendChild(coinBox);
+    });
+  });
